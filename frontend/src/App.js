fd40941045d6a88f46a6c0568a7335561ba5eb9a
Ramith <89182652+ramithperera@users.noreply.github.com>
2021-09-30 16:24:56 +0530

    unwanted line deleted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/storeHeader';
 import AddProduct from './components/AddProduct';
-import AllProducts from './components/AllProducts';
 import editProducts from './components/editProduct';
 import productRead from './components/productRead';
 import { BrowserRouter as Router, Route } from "react-router-dom";
